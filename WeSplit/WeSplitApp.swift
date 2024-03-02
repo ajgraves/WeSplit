8ced2d5cb670ad90adae1de4ee9428e54d15ebb4
Aaron Graves <ajgraves@ajgraves.com>
2024-03-01 20:21:45 -0600

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Aaron Graves on 3/1/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
